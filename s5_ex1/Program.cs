380a257d592c5aaaeaeaa7990febbeee3692e2dd
ElenaShamiryan <elen91@list.ru>
2022-08-10 23:20:22 +0300

    количество четных чисел в массиве равно

---
+++ s5_ex1/Program.cs
@@ -0,0 +1,43 @@
+﻿void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write($"{col[position]} ");
+        position++;
+    }
+}
+
+void FillArray(int[] array)
+{
+    int n = array.Length;
+    int i = 0;
+    while (i < n)
+    {
+        array[i] = new Random().Next(100, 1000);
+        i++;
+    }
+}
+
+Console.WriteLine("Введите количество элементов массива ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+
+int NewArray(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (array[i] % 2 == 0) count = count + 1;
+
+        if (array[i] % 2 != 0 && count == 0) count = 0;
+    }
+    return count;
+}
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+int col = NewArray(array);
+Console.WriteLine($"Количество четных чисел в массиве равно {col}");
